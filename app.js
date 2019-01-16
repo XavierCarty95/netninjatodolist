3111f4f62ba2cfc62b9ed002e6c615558466cba7
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-01-16 23:21:35 +0000

    templete

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-var express = require('express');
-
-
+var express = require("express");
 var app = express();
 
-app.set('view engine' , 'ejs')
+app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
-	res.send("This is the homepage");	
+	res.sendFile(__dirname + "/index.html");	
 });
+
 app.get("/contact", function(req, res){
-	res.send("This is the contact page");	
+	res.sendFile(__dirname + "/contact.html");
 });
 
+app.get("/404", function(req, res){
+	res.sendFile(__dirname + "/404.html");
+});
 app.get("/profile/:name", function(req, res){
-    var data = {age: 29 , job: 'ninja'};
-	res.render('profile' , {person: req.params.name , data:data}  );
-});
+	var data = {age: 29, job: "ninja", hobbies: ["eating", "fighting", "fishing"]};
+	res.render("profile", {person: req.params.name, data: data}); 
+})
 
-app.listen(8080);
+app.listen(8080)
